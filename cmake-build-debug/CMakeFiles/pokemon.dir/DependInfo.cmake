98a85a3b190c00dcb9560cf295374311d050026b
Florian Lafuente <florian.lafuente@gmail.com>
2020-03-16 22:08:30 +0100

    cmake

---
+++ cmake-build-debug/CMakeFiles/pokemon.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/flori/CLionProjects/pokemon/src/Pokemon.cpp" "C:/Users/flori/CLionProjects/pokemon/cmake-build-debug/CMakeFiles/pokemon.dir/src/Pokemon.cpp.obj"
+  "C:/Users/flori/CLionProjects/pokemon/src/Trainer.cpp" "C:/Users/flori/CLionProjects/pokemon/cmake-build-debug/CMakeFiles/pokemon.dir/src/Trainer.cpp.obj"
+  "C:/Users/flori/CLionProjects/pokemon/src/main.cpp" "C:/Users/flori/CLionProjects/pokemon/cmake-build-debug/CMakeFiles/pokemon.dir/src/main.cpp.obj"
+  "C:/Users/flori/CLionProjects/pokemon/src/pokemons/Bulbasaur.cpp" "C:/Users/flori/CLionProjects/pokemon/cmake-build-debug/CMakeFiles/pokemon.dir/src/pokemons/Bulbasaur.cpp.obj"
+  "C:/Users/flori/CLionProjects/pokemon/src/pokemons/Charmander.cpp" "C:/Users/flori/CLionProjects/pokemon/cmake-build-debug/CMakeFiles/pokemon.dir/src/pokemons/Charmander.cpp.obj"
+  "C:/Users/flori/CLionProjects/pokemon/src/pokemons/Squirtle.cpp" "C:/Users/flori/CLionProjects/pokemon/cmake-build-debug/CMakeFiles/pokemon.dir/src/pokemons/Squirtle.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/pokemon.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pokemon.dir/src/Pokemon.cpp.obj"
+  "CMakeFiles/pokemon.dir/src/Trainer.cpp.obj"
+  "CMakeFiles/pokemon.dir/src/main.cpp.obj"
+  "CMakeFiles/pokemon.dir/src/pokemons/Bulbasaur.cpp.obj"
+  "CMakeFiles/pokemon.dir/src/pokemons/Charmander.cpp.obj"
+  "CMakeFiles/pokemon.dir/src/pokemons/Squirtle.cpp.obj"
+  "libpokemon.dll.a"
+  "pokemon.exe"
+  "pokemon.exe.manifest"
+  "pokemon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pokemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
